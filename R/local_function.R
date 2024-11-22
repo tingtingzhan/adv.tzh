33aef1a8e060e1894b4706b2236505baf43c1d02
Tingting Zhan <tingtingzhan@gmail.com>
2024-11-21 21:17:08 -0500

    test

--- R/local_function.R
+++ R/local_function.R
@@ -12,10 +12,10 @@
 #' Need to **Suggests** for examples to run.
 #' 
 #' @examples
-#' local_function('base') # wow
-#' local_function('stats') # wow
-#' local_function('utils') # wow
-#' local_function('tools') # wow
+#' local_function('base')
+#' local_function('stats')
+#' local_function('utils')
+#' local_function('tools')
 #' local_function('MASS')
 #' local_function('nlme')
 #' local_function('survival')
@@ -26,17 +26,45 @@ local_function <- function(pkg, ...) {
   # as.environment(sprintf(fmt = 'package:%s', pkg)) # need to load into search()
   
   # https://stackoverflow.com/questions/12114355/show-names-of-everything-in-a-package
-  x <- ls(name = ns, all.names = TRUE)
+  x <- ls(name = ns, all.names = TRUE, sorted = TRUE)
   
   obj <- mget(x, envir = ns)
-  fn <- obj[vapply(obj, FUN = is.function, FUN.VALUE = NA)]
-  ev <- lapply(fn, FUN = environment)
-  id <- !vapply(ev, FUN = identical, y = ns, FUN.VALUE = NA)
-  if (!any(id)) {
-    message(sprintf(fmt = 'None of %d functions are defined via ?base::local, etc.', length(id)))
-  } else message(sprintf(fmt = '%d/%d (%.1f%%) functions are defined via ?base::local, etc.', sum(id), length(id), 1e2*mean(id)))
+  fn0 <- obj[vapply(obj, FUN = is.function, FUN.VALUE = NA)]
   
-  return(invisible(names(which(id))))
+  # primitive functions (e.g., ?base::`-`) does not have an environment
+  # environment(fun = base::`-`) # NULL
+  fn <- fn0[!vapply(fn0, FUN = is.primitive, FUN.VALUE = NA)]
+  
+  ev <- lapply(fn, FUN = environment)
+  
+  # e.g., ?stats:::update.packageStatus; <environment: namespace:utils>
+  # but \pkg{stats} does *not* import \pkg{utils} 
+  #package_dependencies(pkg) # no use..
+  
+  # functions imported from another package/namespace, e.g., 
+  # (env = environment(stats:::update.packageStatus))
+  # isNamespace(env) # TRUE
+  # isNamespace(environment(spatstat.explore::Emark)) # FALSE
+  id <- !vapply(ev, FUN = isNamespace, FUN.VALUE = NA) & !vapply(ev, FUN = identical, y = ns, FUN.VALUE = NA)
+  if (!any(id)) {
+    message(sprintf(fmt = 'None of %d functions in package \033[1;35m%s\033[0m are defined via ?base::local, etc.', length(id), pkg))
+    return(invisible())
+  } 
+  
+  lapply(names(which(id)), FUN = function(f) { # (f = '-')
+    env <- environment(fun = get(f, envir = ns))
+    x <- setdiff(ls(envir = env, all.names = TRUE), y = f)
+    message(sprintf(
+      fmt = 'Local envir of \033[1;34m%s\033[0m contains %s', f,
+      if (length(x)) {
+        paste0('\033[1;33m', x, '\033[0m', collapse = ', ')
+      } else '\033[1;32mnothing else\033[0m'
+    ))
+  })
+  message()
+  message(sprintf(fmt = '\033[1;32m%d/%d; %.1f%%\033[0m functions in package \033[1;36m%s\033[0m are defined via ?base::local, etc.', sum(id), length(id), 1e2*mean(id), pkg))
+
+  return(invisible())
   
 }
 
