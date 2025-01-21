7639ce1571c69b2c0db0e5a78a1e2af168f0b035
Tingting Zhan <tingtingzhan@gmail.com>
2025-01-21 11:05:52 -0500

    test

--- R/local_function.R
+++ R/local_function.R
@@ -6,7 +6,7 @@
 #' @param ... additional parameters, currently not in use
 #' 
 #' @returns 
-#' Function [local_function] returns an \link[base]{invisible} \link[base]{numeric} \link[base]{vector}.
+#' Function [local_function] does not have a returned value.
 #' 
 #' @note
 #' Need to **Suggests** for examples to run.
@@ -19,6 +19,7 @@
 #' local_function('MASS')
 #' local_function('nlme')
 #' local_function('survival')
+#' @name local_function
 #' @importFrom cli style_bold col_green col_yellow col_blue col_magenta col_cyan
 #' @export
 local_function <- function(pkg, ...) {
@@ -28,50 +29,136 @@ local_function <- function(pkg, ...) {
   
   # https://stackoverflow.com/questions/12114355/show-names-of-everything-in-a-package
   x <- ls(name = ns, all.names = TRUE, sorted = TRUE)
+  fn <- x[vapply(mget(x, envir = ns), FUN = is.function, FUN.VALUE = NA)]
   
-  obj <- mget(x, envir = ns)
-  fn0 <- obj[vapply(obj, FUN = is.function, FUN.VALUE = NA)]
+  ret <- lapply(fn, FUN = function(i) { # (i = fn[[1L]])
+    eval(call(name = 'print_local_obj', call(name = ':::', as.name(pkg), as.name(i))))
+  })
   
-  # primitive functions (e.g., ?base::`-`) does not have an environment
-  # environment(fun = base::`-`) # NULL
-  fn <- fn0[!vapply(fn0, FUN = is.primitive, FUN.VALUE = NA)]
+  id <- (lengths(ret, use.names = FALSE) > 0L)
   
-  ev <- lapply(fn, FUN = environment)
-  
-  # e.g., ?stats:::update.packageStatus; <environment: namespace:utils>
-  # but \pkg{stats} does *not* import \pkg{utils} 
-  #package_dependencies(pkg) # no use..
-  
-  # functions imported from another package/namespace, e.g., 
-  # (env = environment(stats:::update.packageStatus))
-  # isNamespace(env) # TRUE
-  # isNamespace(environment(spatstat.explore::Emark)) # FALSE
-  id <- !vapply(ev, FUN = isNamespace, FUN.VALUE = NA) & !vapply(ev, FUN = identical, y = ns, FUN.VALUE = NA)
   if (!any(id)) {
     message(sprintf(fmt = 'None of %d functions in package %s are defined via ?base::local, etc.', length(id), style_bold(col_magenta(pkg))))
     return(invisible())
   } 
   
-  lapply(names(which(id)), FUN = function(f) { # (f = '-')
-    env <- environment(fun = get(f, envir = ns))
-    x <- setdiff(ls(envir = env, all.names = TRUE), y = f)
-    message(sprintf(
-      fmt = 'Local envir of %s contains %s', 
-      style_bold(col_blue(f)),
-      if (length(x)) {
-        paste0(style_bold(col_yellow(x)), collapse = ', ')
-      } else style_bold(col_green('nothing else'))
-    ))
-  })
+  message(paste(unlist(ret[id]), collapse = '\n'))
   message()
   message(sprintf(
     fmt = '%s functions in package %s are defined via ?base::local, etc.', 
     style_bold(col_green(sprintf(fmt = '%d/%d; %.1f%%', sum(id), length(id), 1e2*mean(id)))), 
     style_bold(col_cyan(pkg))))
-
+  
   return(invisible())
   
 }
+
+
+
+#' @rdname local_function
+#' 
+#' @param fun \link[base]{function}, must be given in the format of `pkg::function`
+#' 
+#' @details
+#' Helper function [.local_obj] provides the names of other objects 
+#' in the \link[base]{local} environment of function `fun` definition.
+#' 
+#' @returns
+#' Helper function [.local_obj] returns a \link[base]{character} \link[base]{vector}.
+#' 
+#' @examples
+#' .local_obj(fun = base::sum) # primitive
+#' .local_obj(fun = base::sub) # not defined in local environment
+#' .local_obj(fun = stats:::update.packageStatus) # actually ?utils:::update.packageStatus
+#' .local_obj(fun = base::.doSortWrap)
+#' @export
+.local_obj <- function(fun) {
+  
+  fun.name <- substitute(fun)
+  if (!(as.character(fun.name[[1L]]) %in% c('::', ':::'))) stop('`fun` must be pkg::fun format')
+  
+  # primitive functions (e.g., ?base::`-`) does not have an environment
+  # environment(fun = base::`-`) # NULL
+  if (is.primitive(fun)) return(invisible())
+  
+  env <- environment(fun = fun)
+  
+  if (is.null(env)) stop('other than primitive function?')
+  
+  if (isNamespace(env)) return(invisible()) # ?base::getNamespace of this packge, or some other packages
+  
+  setdiff(ls(envir = env, all.names = TRUE), y = as.character(fun.name[[3L]]))
+  
+}
+
+#' @rdname local_function
+#' 
+#' @details
+#' Helper function [print_local_obj] prints the returned value of function [.local_obj]
+#' in a pretty fashion (with the help of package \CRANpkg{cli}).
+#' 
+#' @returns 
+#' Helper function [print_local_obj] returns an ANSI string, 
+#' see \link[cli]{ansi-styles}.
+#' 
+#' @examples
+#' message(print_local_obj(fun = base::.doSortWrap))
+#' @export
+print_local_obj <- function(fun) {
+  
+  x <- eval(call(name = '.local_obj', fun = substitute(fun)))
+  if (!length(x)) return(invisible())
+  
+  return(sprintf(
+    fmt = 'Local envir of %s contains %s', 
+    style_bold(col_blue(as.character(substitute(fun)[[3L]]))),
+    if (length(x)) {
+      paste0(style_bold(col_yellow(x)), collapse = ', ')
+    } else style_bold(col_green('nothing else'))
+  ))
+  
+} 
+
+
+#' @rdname local_function
+#' 
+#' @param envir an \link[base]{environment} to load the \link[base]{local} objects.
+#' Default `.GlobalEnv`
+#' 
+#' @details
+#' Helper function [load_local_obj] loads the returned value of function [.local_obj]
+#' into a user-specified \link[base]{environment}.
+#' 
+#' @returns 
+#' Helper function [load_local_obj] does not have a returned value
+#' 
+#' @examples
+#' load_local_obj(fun = base::.doSortWrap)
+#' @export
+load_local_obj <- function(fun, envir = .GlobalEnv) {
+  
+  x <- eval(call(name = '.local_obj', fun = substitute(fun)))
+  if (!length(x)) return(invisible())
+  
+  from <- environment(fun = fun)
+  lapply(x, FUN = function(i) { # (i = x[[1L]])
+    assign(x = i, value = get(x = i, envir = from), envir = envir)
+  })
+  
+  return(invisible())
+  
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 if (FALSE) {
